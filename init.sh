bdd37453701ba882ebc24080e640b8a5a26d956d
ichenhanlin <ichenhanlin@outlook.com>
2022-03-27 17:08:58 +0800

    change plugins manager to packer.nvim

--- init.sh
+++ init.sh
@@ -1,13 +1,10 @@
-#!/bin/sh
-
-OS="ubuntu"
+#!/bin/bash
 
 getOS(){
-
     if [[ "$OSTYPE" =~ "darwin" ]]; then
         echo "mac"
     elif [[ "$OSTYPE" =~ "linux" ]]; then
-        source /etc/os-relase
+        source /etc/os-release
         case $ID in
             debian|ubuntu)
                 echo "ubuntu"
@@ -21,50 +18,52 @@ getOS(){
     fi
 }
 
-zhsInstall(){
-    # install Neovim
+install(){
     if [ $OS == "ubuntu" ]; then
-        sudo apt-get install -y zsh
+        sudo apt-get install -y zsh ripgrep curl build-essential
     elif [ $OS == "centos" ]; then
-        sudo yum install -y zsh
+        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+        sudo yum install -y zsh ripgrep curl build-essential
     fi
 
+    # require neovim >= 0.61
+    if [ $OS == 'mac' ]; then
+        # homebrew will install latest version
+        brew install neovim ripgrep
+    else
+        # apt and yum may be old version
+        curl -O -L "https://github.com/neovim/neovim/releases/download/v0.6.1/nvim.appimage"
+        chmod +x nvim.appimage
+        sudo mv ./nvim.appimage /usr/local/bin/nvim
+    fi
+}
+
+zhsInstall(){
     mkdir -p ~/.config/zsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    export RUNZSH=no
+    yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -i
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
-    wait
 
     cat >> ${ZDOTDIR:-$HOME}/.zshrc<<EOF
-        alias vim="nvim"
-        alias vi="nvim"
-        source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-        source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+alias vim="nvim"
+alias vi="nvim"
+source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 EOF
-    
-    echo "Install zsh and Oh My Zsh done!"
 }
 
 neovimInstall(){
-    # install Neovim
-    if [ $OS == "mac" ]; then
-        brew install neovim
-        brew install ripgrep
-    elif [ $OS == "ubuntu" ]; then
-        sudo apt-get install -y ripgrep neovim
-    elif [ $OS == "centos" ]; then
-        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-        sudo yum install -y ripgrep neovim
-    fi
-
     # install vim-plug
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+    # packer.vim
+    # git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    #     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
     # init neovim config
     ln -s ${HOME}/dotFiles/nvim ${HOME}/.config/nvim
-    echo "Install Neovim done!"
 }
 
 goInstall(){
@@ -79,19 +78,24 @@ goInstall(){
     mkdir -p ${HOME}/workspace/golang
 
     cat >> ${ZDOTDIR:-$HOME}/.zshrc<<EOF
-        export GOPATH=${HOME}/workspace/golang
-        export GOBIN=${GOPATH}/bin
-        export PATH=$PATH:$GOBIN
+export GOPATH=${HOME}/workspace/golang
+export GOBIN=${GOPATH}/bin
+export PATH=${PATH}:${GOBIN}
 EOF
 
-    // install gopls
+    # install gopls
+    export GOPATH=${HOME}/workspace/golang 
     go get golang.org/x/tools/gopls
 }
+
 OS=`getOS`
 if [ $OS == "Unspported OS" ]; then
     echo "Unsupported OS"
     exit 1
 fi
+install
 zhsInstall
 neovimInstall
 goInstall
+
+exec zsh -l
