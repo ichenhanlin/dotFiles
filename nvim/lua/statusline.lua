9b79105b88acefceb73672695a462ed27b1c3c23
ichenhanlin <ichenhanlin@outlook.com>
2022-08-14 22:20:16 +0800

    beautify the status line

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,10 +1,91 @@
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = " ", warn = " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = "  ", modified = " ", removed = " " },
+  diff_color = {
+    added = { fg = "#98be65" },
+    modified = { fg = "#ecbe7b" },
+    removed = { fg = "#ec5f67" },
+  },
+  cond = hide_in_width
+}
+
+local file_name = {
+  'filename',
+  file_status = true, -- Displays file status (readonly status, modified status)
+  path = 1, -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+
+  shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+  -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = '[+]', -- Text to show when the file is modified.
+    readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+  },
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = false,
+  icon = nil,
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "",
+}
+
+local location = {
+  "location",
+  padding = 0,
+}
+
+-- cool function for progress
+local progress = function()
+  local current_line = vim.fn.line(".")
+  local total_lines = vim.fn.line("$")
+  -- local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local chars = { "██", "▇▇", "▆▆", "▅▅", "▄▄", "▃▃", "▂▂", "▁▁", " ", }
+  local line_ratio = current_line / total_lines
+  local index = math.ceil(line_ratio * #chars)
+  return chars[index]
+end
+
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
 require('lualine').setup {
+    option = {
+        disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+    },
+    sections = {
+        lualine_a = { 'mode', branch },
+        lualine_b = { diagnostics },
+        lualine_c = { file_name },
+        lualine_x = { diff, spaces, "encoding", filetype, "fileformat" },
+        lualine_y = { location },
+        lualine_z = { progress },
+    },
     tabline = {
-      lualine_a = {'buffers'},
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {'tabs'}
+        lualine_a = {'buffers'},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
     }
 }
