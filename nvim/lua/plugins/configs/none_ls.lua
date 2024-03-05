736a38f72b3c87ddd142b0332f577902d8bed9b8
mateochen <mateochen@tencent.com>
2024-03-05 16:58:33 +0800

    feat: add goimports formatting

--- nvim/lua/plugins/configs/none_ls.lua
+++ nvim/lua/plugins/configs/none_ls.lua
@@ -5,6 +5,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.code_actions.gomodifytags,
